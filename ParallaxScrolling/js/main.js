38781461a95d63954978800185c357b5b1ec8509
JuanMolfese <juanjosemolfese@gmail.com>
2022-06-25 17:04:21 -0300

    se deja funcionando lectura de barra espaciadora en js.

---
+++ ParallaxScrolling/js/main.js
@@ -0,0 +1,16 @@
+"use strict"
+/* let char = document.getElementById("char"); */
+/* let keydown = document.keyDown(); */
+
+window.addEventListener("keydown", function (event) {
+  
+    if (event.key == " ") {
+      console.log("ok");
+    }
+  });
+
+/* char.style="char_jump"; */
+
+
+
+
