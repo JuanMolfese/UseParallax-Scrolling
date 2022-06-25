4da96fec8576d83f8bad620213539443373a43b3
JuanMolfese <juanjosemolfese@gmail.com>
2022-06-25 20:21:16 -0300

    queda andando la accion saltar. se hacen pruebas con morir pero a men que puse forward luego de terminar la animacion vuelve y permanece en el primer frame.

--- ParallaxScrolling/js/main.js
+++ ParallaxScrolling/js/main.js
@@ -1,16 +1,37 @@
 "use strict"
-/* let char = document.getElementById("char"); */
-/* let keydown = document.keyDown(); */
+let keydown = null;
+var gameOver = false;
 
-window.addEventListener("keydown", function (event) {
-  
-    if (event.key == " ") {
-      console.log("ok");
+if (!gameOver){
+    /* var char = document.querySelectorAll("div.char_run, div.char_death, div.char_jump"); */
+    var char = document.querySelector("#char");
+    var fondo = document.querySelectorAll("div.fondo");
+
+    console.log(fondo);
+
+    function jump(){
+        char.classList.remove("char_jump"); 
+        char.classList.add("char_run");
     }
-  });
-
-/* char.style="char_jump"; */
 
 
+    //EVENTOS DE TECLA --  por ej BARRA ESPACIADORA PARA SALTAR// 
+    window.addEventListener("keydown", function (event) {     
+        
+        if (event.key == " ") {
+            char.classList.remove("char_run");
+            char.classList.remove("char_death");
+            char.classList.add("char_jump");
+            setTimeout(jump,1300);
+        }
+        else if (event.key == "d") {  //EN CASO DE MORIR, ahora prueb ocon la tecla D
+             
+            char.classList.add("char_death");
+            gameOver = true;            
+            detenerFondo();
+            cartelAviso("Perdiste");            
+        }
+        }
+    );
 
-
+}
