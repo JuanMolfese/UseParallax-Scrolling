ec11a5193ba439b78f489de9a6834dc452ce49a8
JuanMolfese <juanjosemolfese@gmail.com>
2022-07-01 12:25:40 -0300

    se crea clase Char

---
+++ ParallaxScrolling/js/class/char.js
@@ -0,0 +1,41 @@
+class Char{
+    constructor (){
+        char_live = true;
+        char_onJump = false;
+        char_coins = 0;
+        char_speed = 1;
+        char_invensible = false;
+    }
+
+    //si armamos clases aca tendriamos que mover el jump, el death
+    /* function jump(){
+        this.char.classList.remove("char_jump"); 
+        this.char.classList.add("char_run");
+    }
+
+     function death() {
+        char.classList.add("char_death");
+    }
+    
+    function pickCoin(){
+        char_coins = char_coins +1;
+    }
+
+    function invensible(){
+        char_invensible = true;
+    }
+
+    function no_invensible(){
+        char_invensible = false;
+    }
+
+    function superSpeed(){
+        char.style.animation-duration = 0.4s; //el fondo tmb tendria q alterar su velocidad
+    }
+    
+    function normalSpeed(){
+        char.style.animation-duration = 0.7s; //el fondo tmb tendria q alterar su velocidad
+    }
+    
+    */
+}
