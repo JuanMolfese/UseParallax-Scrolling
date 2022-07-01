5da2d52d1eb490f8def46eadadf9e08d372c23b5
JuanMolfese <juanjosemolfese@gmail.com>
2022-06-30 21:41:13 -0300

    se agregan carteles de ganar perder, se cambia position de container (relative) y de los items (absolute). Se le suman acciones a la muerte. se detiene el fondo ....muy abruptamente...seguir investigando y aparece cartel LOOSE

--- ParallaxScrolling/js/main.js
+++ ParallaxScrolling/js/main.js
@@ -6,14 +6,35 @@ if (!gameOver){
     /* var char = document.querySelectorAll("div.char_run, div.char_death, div.char_jump"); */
     var char = document.querySelector("#char");
     var fondo = document.querySelectorAll("div.fondo");
+    var cartel_loose = document.querySelector("#cartel_loose");
+    var layer1 = document.querySelector(".layer-1");
+    var layer2 = document.querySelector(".layer-2");
+    var layer3 = document.querySelector(".layer-3");
+    var layer4 = document.querySelector(".layer-4");
+    var layer5 = document.querySelector(".layer-5");
+    var layer6 = document.querySelector(".layer-6");
 
+    
     console.log(fondo);
 
+    
     function jump(){
         char.classList.remove("char_jump"); 
         char.classList.add("char_run");
     }
 
+    function mostrar_cartel_loose(){
+        cartel_loose.style.visibility='visible';
+    }
+
+    function detener_fondo(){
+        layer1.style.animation="pause";
+        layer2.style.animation="pause";
+        layer3.style.animation="pause";
+        layer4.style.animation="pause";
+        layer5.style.animation="pause";
+        layer6.style.animation="pause";
+    }
 
     //EVENTOS DE TECLA --  por ej BARRA ESPACIADORA PARA SALTAR// 
     window.addEventListener("keydown", function (event) {     
@@ -28,9 +49,9 @@ if (!gameOver){
              
             char.classList.add("char_death");
             gameOver = true;            
-            detenerFondo();
-            cartelAviso("Perdiste");            
-        }
+            this.setTimeout(detener_fondo, 50);
+            this.setTimeout(mostrar_cartel_loose,1200);            
+        }       
         }
     );
 
