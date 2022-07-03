8128ec787f8307e470f160ea535acc4df7ec43b3
JuanMolfese <juanjosemolfese@gmail.com>
2022-07-02 21:31:12 -0300

    se agrega coin counter y boton go_back

--- ParallaxScrolling/js/main.js
+++ ParallaxScrolling/js/main.js
@@ -3,10 +3,12 @@ let keydown = null;
 var gameOver = false;
 
 if (!gameOver){
-    /* var char = document.querySelectorAll("div.char_run, div.char_death, div.char_jump"); */
+    var container = document.querySelector('.container');
     var char = document.querySelector("#char");
     var fondo = document.querySelectorAll("div.fondo");
     var cartel_loose = document.querySelector("#cartel_loose");
+    var cartel_win = document.querySelector("#cartel_win");
+    var coins_counter = 3;
     var layer1 = document.querySelector(".layer-1");
     var layer2 = document.querySelector(".layer-2");
     var layer3 = document.querySelector(".layer-3");
@@ -14,17 +16,25 @@ if (!gameOver){
     var layer5 = document.querySelector(".layer-5");
     var layer6 = document.querySelector(".layer-6");
 
-    
-    console.log(fondo);
+    let coin_counter_div = document.createElement('div');
+    coin_counter_div.id= "coin_counter";
+    coin_counter_div.innerHTML = '<p>'+`${coins_counter}`+'</p>';
+    container.appendChild(coin_counter_div);
+    document.body.appendChild(container);
 
-    
+    console.log(coins_counter);
+
     function jump(){
         char.classList.remove("char_jump"); 
-        char.classList.add("char_run");
+        char.classList.add("char_run");        
     }
 
     function mostrar_cartel_loose(){
         cartel_loose.style.visibility='visible';
+    }
+
+    function mostrar_cartel_win(){
+        cartel_win.style.visibility='visible';
     }
 
     function detener_fondo(){
@@ -51,6 +61,10 @@ if (!gameOver){
             gameOver = true;            
             this.setTimeout(detener_fondo, 1200);
             this.setTimeout(mostrar_cartel_loose,1200);            
+        }
+        else if (event.key == "w") {  //EN CASO DE GANAR, ahora prueb ocon la tecla W
+             
+            this.setTimeout(mostrar_cartel_win,1200);            
         }       
         }
     );
